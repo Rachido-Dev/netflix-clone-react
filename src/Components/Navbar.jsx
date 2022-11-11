d5ac336ddfd6be5bd7c0195bf62f064cab2566cd
Rachid <116731861+dev-rachido@users.noreply.github.com>
2022-11-11 09:18:50 +0100

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='text-white nav__container flex items-center justify-between p-4 z-[100] w-full absolute'>
-        <h1 className='text-red-800 text-6xl font-bold font-sans cursor-pointer'>Netflix</h1>
+        <h1 className='text-red-800 text-6xl font-bold font-sans cursor-pointer'>Rachidflix</h1>
         <div className="log__block">
         <button className="bg-white px-6 py-2 text-red-400 ">Sign In</button>
         <button className='bg-red-600 px-6 py-2'>Register</button>
